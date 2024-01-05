51935c18b04885f9d08f2d668094dc5607324f10
Anna Nechaeva <115800504+a-nechaeva@users.noreply.github.com>
2024-01-05 04:03:44 +0300

    Update electric_move.py

--- python_code/electric_move.py
+++ python_code/electric_move.py
@@ -3,13 +3,13 @@ import matplotlib.pyplot as plt
 
 
 # ---- начальные условия ---- #
-v_0 = 1  # м/с
+v_0 = 0.1  # м/с
 alpha = pi / 6  # угол между вектором скорости и x
 x_0 = 3  # м
 y_0 = 5  # м
-m = 0.001  # кг
-q = 1  # Кл
-Q = 7  # Кл
+m = 0.000001  # кг
+q = 0.001  # Кл
+Q = 0.07  # Кл
 eps_0 = 8.85 * 10 ** (-12)
 k = 1 / (4 * pi * eps_0)  # коэффициент
 
@@ -49,19 +49,19 @@ def y_frst(y_first, y_second):
 
 # ---- функция, отсчитывающая x'' ---- #
 def x_scnd(x, y):
-    return -(k * q * Q / m) * x * (x ** 2 + y ** 2) ** (-3 / 2)
+    return -(k * q * Q / m) * x * ((x ** 2 + y ** 2) ** (-3 / 2))
 
 
 # ---- функция, отсчитывающая y'' ---- #
 def y_scnd(x, y):
-    return -(k * q * Q / m) * x * (x ** 2 + y ** 2) ** (-3 / 2)
+    return -(k * q * Q / m) * y * ((x ** 2 + y ** 2) ** (-3 / 2))
 
 
 # ---- основная функция подсчета ---- #
 def constructor():
     for i in range(10000):
         T_array.append(t_counter(T_array[-1]))
-        X_array.append(x_counter(X_array[-1], Vy_array[-1]))
+        X_array.append(x_counter(X_array[-1], Vx_array[-1]))
         Y_array.append(y_counter(Y_array[-1], Vy_array[-1]))
         Vx_array.append(x_frst(Vx_array[-1], x_scnd(Vx_array[-1], Vy_array[-1])))
         Vy_array.append(y_frst(Vy_array[-1], y_scnd(Vx_array[-1], Vy_array[-1])))
@@ -73,7 +73,7 @@ Tuple = constructor()
 
 
 # ---- строим график ----#
-plt.axis([-6.0, 6.0, -6.0, 6.0])
+#plt.axis([-6.0, 6.0, -6.0, 6.0])
 # ---- добавляем подписи к осям и заголовок диаграммы ----#
 plt.xlabel('x, м', fontsize=16)
 plt.ylabel('y, м', fontsize=16)
