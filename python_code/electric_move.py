45763590c1ab5bc8f75097e033517b732397010c
Anna Nechaeva <115800504+a-nechaeva@users.noreply.github.com>
2024-01-05 20:44:25 +0300

    upd

--- python_code/electric_move.py
+++ python_code/electric_move.py
@@ -13,8 +13,9 @@ Q = 0.07  # Кл
 eps_0 = 8.85 * 10 ** (-12)
 k = 1 / (4 * pi * eps_0)  # коэффициент
 
+
 # ---- параметры для моделирования ---- #
-dt = 0.01  # с
+dt = 0.0001  # с
 T_array = [0]  # список, хранящий время
 X_array = [x_0]  # список, хранящий итерации по x
 Y_array = [y_0]  # список, хранящий итерации по y
@@ -29,42 +30,54 @@ def t_counter(t):
 
 # ---- функция, отсчитывающая x ---- #
 def x_counter(x, x_first):
-    return x + dt * x_first
+    a = x + dt * x_first
+    print("X: ", a, "\n")
+    return a
 
 
 # ---- функция, отсчитывающая y ---- #
 def y_counter(y, y_first):
-    return y + dt * y_first
+    a = y + dt * y_first
+    print("Y: ", a, "\n")
+    return a
 
 
 # ---- функция, отсчитывающая x' ---- #
 def x_frst(x_first, x_second):
-    return x_first + dt * x_second
+    a = x_first + dt * x_second
+    print("Vx: ", a, "\n")
+    return a
 
 
 # ---- функция, отсчитывающая y' ---- #
 def y_frst(y_first, y_second):
-    return y_first + dt * y_second
+    a = y_first + dt * y_second
+    print("Vy: ", a, "\n")
+    return a
 
 
 # ---- функция, отсчитывающая x'' ---- #
 def x_scnd(x, y):
-    return -(k * q * Q / m) * x * ((x ** 2 + y ** 2) ** (-3 / 2))
+    a = -(k * q * Q / m) * x * ((x ** 2 + y ** 2) ** (-3 / 2))
+    print("Ax: ", a, "\n")
+    return a
 
 
 # ---- функция, отсчитывающая y'' ---- #
 def y_scnd(x, y):
-    return -(k * q * Q / m) * y * ((x ** 2 + y ** 2) ** (-3 / 2))
+    a = -(k * q * Q / m) * y * ((x ** 2 + y ** 2) ** (-3 / 2))
+    print("Ay: ", a, "\n")
+    return a
 
 
 # ---- основная функция подсчета ---- #
 def constructor():
-    for i in range(100):
+    for i in range(10):
         T_array.append(t_counter(T_array[-1]))
+        Vx_array.append(x_frst(Vx_array[-1], x_scnd(X_array[-1], Y_array[-1])))
+        Vy_array.append(y_frst(Vy_array[-1], y_scnd(X_array[-1], Y_array[-1])))
         X_array.append(x_counter(X_array[-1], Vx_array[-1]))
         Y_array.append(y_counter(Y_array[-1], Vy_array[-1]))
-        Vx_array.append(x_frst(Vx_array[-1], x_scnd(Vx_array[-1], Vy_array[-1])))
-        Vy_array.append(y_frst(Vy_array[-1], y_scnd(Vx_array[-1], Vy_array[-1])))
 
     return X_array, Y_array
 
@@ -87,6 +100,11 @@ plt.tight_layout()
 center_Q = 0.0, 0.0
 c_Q = plt.Circle(center_Q, radius=1.5)
 plt.gca().add_artist(c_Q)
+plt.xlim(-5, 5)  # Новый масштаб оси X от 0 до 6
+plt.ylim(-5, 5)
 plt.show()
 
-print(Vy_array)
+print("Vx: ", Vx_array, "\n")
+print("Vy: ", Vy_array, "\n")
+print("X: ", X_array, "\n")
+print("Y: ", Y_array, "\n")
